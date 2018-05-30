51e31e4f6be64c54940a95f7c45c0fa90694517c
Kohdai Hirasawa <hrswkhd@gmail.com>
2018-05-30 21:57:49 +0900

    firstcommit

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,31 @@
+class MicropostsController < ApplicationController
+
+before_action :logged_in_user, only:[:new,:create,:destroy]
+
+  def new
+    @micropost = current_user.microposts.build
+  end
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "投稿完了しました。"
+      redirect_to users_path
+    end
+  end
+
+  def index
+    @microposts = Micropost.all
+  end
+
+  def destroy
+    Micropost.find(params[:id]).destroy
+    flash[:success] = "削除しました"
+    redirect_to request.referrer || root_url
+  end
+
+  private
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,75 @@
+class UsersController < ApplicationController
+
+  before_action :logged_in_user, only:[:index,:edit,:destroy]
+  before_action :correct_user, only:[:edit,:update,:destroy]
+
+  #get users#new
+  def new
+    @user = User.new
+    #form_for(@user)
+  end
+
+  #post users#new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #success
+      log_in @user
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to users_path
+    else
+      #failure
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts
+  end
+
+  #get users
+  def index
+    @users = User.all
+    #from_for @users
+  end
+
+  #get users#edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  #post users#edit
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  #post users#destroy
+  def destroy
+
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :name, :email, :password,
+        :password_confirmation)
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to users_path unless @user == @current_user
+    end
+
+    def admin_user
+    end
+end
