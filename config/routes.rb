f7be58974050ffdc7261e21985f71b1721290012
Kohdai Hirasawa <hrswkhd@gmail.com>
2018-06-02 08:49:13 +0900

    likecomprete

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   delete '/logout', to:'sessions#destroy'
   resources :users
   resources :microposts
+  resources :likes, only: [:create, :destroy]
 end
---
+++ db/migrate/20180531121417_create_likes.rb
@@ -0,0 +1,13 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.integer :user_id
+      t.integer :micropost_id
+      t.index :user_id
+      t.index :micropost_id
+      t.index [:user_id, :micropost_id], unique: true
+
+      t.timestamps
+    end
+  end
+end
